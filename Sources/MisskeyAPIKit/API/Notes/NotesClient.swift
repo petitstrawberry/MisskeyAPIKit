92722deec10df79baecffd71049f8962af1fc7be
PetitStrawberry <deloreantaro@yahoo.co.jp>
2023-07-21 15:26:03 +0900

    add: ThreadMuting into NotesClient

--- Sources/MisskeyAPIKit/API/Notes/NotesClient.swift
+++ Sources/MisskeyAPIKit/API/Notes/NotesClient.swift
@@ -11,11 +11,13 @@ public class NotesClient: BaseClient {
     public let favorites: Favorites
     public let polls: Polls
     public let reactions: Reactions
+    public let threadMuting: ThreadMuting
 
     override init(client: MisskeyAPI.Client) {
         favorites = .init(client: client)
         polls = .init(client: client)
         reactions = .init(client: client)
+        threadMuting = .init(client: client)
 
         super.init(client: client)
     }
