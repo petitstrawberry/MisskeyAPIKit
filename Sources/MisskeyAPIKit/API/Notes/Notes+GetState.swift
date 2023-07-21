24f4af8cb0d5b679b19c05e0c173e5e6a591bded
PetitStrawberry <deloreantaro@yahoo.co.jp>
2023-07-21 14:15:44 +0900

    impl: notes/state

---
+++ Sources/MisskeyAPIKit/API/Notes/Notes+GetState.swift
@@ -0,0 +1,39 @@
+//
+//  Notes+GetState.swift
+//  MisskeyAPIKit
+//
+//  Created by petitstrawberry on 2023/07/21.
+//
+
+import Alamofire
+import Foundation
+
+public extension NotesRequest {
+    struct GetStateRequest: BaseRequest {
+        public let endpoint: String = "notes/state"
+        public let params: [String: Any?]?
+
+        init(
+            noteId: String
+        ) {
+            params = [
+                "noteId": noteId,
+            ]
+        }
+    }
+}
+
+public extension NotesResponse {
+    struct GetStateResponse: Codable {
+        public let isFavorited: Bool
+        public let isWatching: Bool
+        public let isMutedThread: Bool
+    }
+}
+
+public extension NotesClient {
+    /// Get note's state
+    func getState(_ request: NotesRequest.GetStateRequest) async throws -> NotesResponse.GetStateResponse {
+        return try await super.request(request)
+    }
+}
