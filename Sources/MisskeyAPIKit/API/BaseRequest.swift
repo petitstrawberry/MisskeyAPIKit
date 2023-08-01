ac7c84bbaa7a25c7c613f7c713de647362bfc4a4
PetitStrawberry <deloreantaro@yahoo.co.jp>
2023-08-01 17:06:55 +0900

    change: BaseRequestをより細かく設定できるように

--- Sources/MisskeyAPIKit/API/BaseRequest.swift
+++ Sources/MisskeyAPIKit/API/BaseRequest.swift
@@ -10,5 +10,17 @@ import Foundation
 
 public protocol BaseRequest {
     var endpoint: String { get }
+    var method: HTTPMethod { get }
     var params: [String: Any?]? { get }
+    var encoding: ParameterEncoding { get }
+    var headers: HTTPHeaders? { get }
+}
+
+public extension BaseRequest {
+    var method: HTTPMethod { .post }
+    var params: [String: Any?]? { nil }
+    var encoding: ParameterEncoding { JSONEncoding.default }
+    var headers: HTTPHeaders? { [
+        "Content-Type": "application/json",
+    ] }
 }
