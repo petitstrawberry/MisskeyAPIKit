0c6df19b432e5e5e532a97dced944c854363c35f
PetitStrawberry <deloreantaro@yahoo.co.jp>
2023-08-01 17:04:22 +0900

    add: drive/files to API

--- Sources/MisskeyAPIKit/API/Drive/DriveClient.swift
+++ Sources/MisskeyAPIKit/API/Drive/DriveClient.swift
@@ -6,7 +6,10 @@
 //
 
 public class DriveClient: BaseClient {
+    public let files: DriveClient.Files
+
     override init(client: MisskeyAPI.Client) {
+        files = Files(client: client)
         super.init(client: client)
     }
 }
