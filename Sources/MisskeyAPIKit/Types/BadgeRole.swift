27295a0fc72646200dbd84bbcbafcd8184219e0f
PetitStrawberry <deloreantaro@yahoo.co.jp>
2023-10-16 18:37:39 +0900

    def: BadgeRole

---
+++ Sources/MisskeyAPIKit/Types/BadgeRole.swift
@@ -0,0 +1,12 @@
+//
+//  BadgeRole.swift
+//  MisskeyAPIKit
+//
+//  Created by petitstrawberry on 2023/10/16.
+//
+
+public struct BadgeRole: Codable {
+    public let name: String
+    public let iconUrl: String?
+    public let displayOrder: Int
+}
