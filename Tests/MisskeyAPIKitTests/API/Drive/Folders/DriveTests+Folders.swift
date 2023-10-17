f767c984e63373b95e337655bbedb8f8c086968b
PetitStrawberry <deloreantaro@yahoo.co.jp>
2023-10-17 12:46:04 +0900

    impl: drives/folders/find

--- Tests/MisskeyAPIKitTests/API/Drive/Folders/DriveTests+Folders.swift
+++ Tests/MisskeyAPIKitTests/API/Drive/Folders/DriveTests+Folders.swift
@@ -7,6 +7,7 @@ extension DriveTests {
     final class FoldersTests: XCTestCase {
         let baseURL = "https://mi.example.com"
 
+        // create
         func testCreate() async throws {
             let configuration = URLSessionConfiguration.af.default
             configuration.protocolClasses = [MockingURLProtocol.self]
@@ -33,5 +34,33 @@ extension DriveTests {
             XCTAssertNotNil(folder)
             XCTAssertEqual(folder.name, "TEST")
         }
+    }
+
+    // find
+    func testFind() async throws {
+        let configuration = URLSessionConfiguration.af.default
+        configuration.protocolClasses = [MockingURLProtocol.self]
+        let sessionManager = Alamofire.Session(configuration: configuration)
+
+        let client = MisskeyAPI(
+            baseURL: baseURL,
+            credentials: .init(accessToken: "access_token"),
+            session: sessionManager
+        )
+
+        let mock = try Mock(url: URL(string: baseURL + "/api/drive/folders/find")!,
+                            dataType: .json,
+                            statusCode: 200,
+                            data: [
+                                .post: Data(contentsOf: TestResources.driveFolders1JSON),
+                            ])
+        mock.register()
+
+        let folders = try await client.drive.folders.find(
+            .init(name: "ai-chan")
+        )
+
+        XCTAssertNotNil(folders)
+        XCTAssert(folders.allSatisfy({ $0.name == "ai-chan" }))
     }
 }
