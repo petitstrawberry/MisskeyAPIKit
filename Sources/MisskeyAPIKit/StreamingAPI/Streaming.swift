441c6e7d8902baf88a2988f4cf381bd8b5791c00
PetitStrawberry <deloreantaro@yahoo.co.jp>
2023-10-27 14:21:19 +0900

    impl: onEvent functions

--- Sources/MisskeyAPIKit/StreamingAPI/Streaming.swift
+++ Sources/MisskeyAPIKit/StreamingAPI/Streaming.swift
@@ -14,14 +14,69 @@ public struct Streaming {
     public var hybridTimelineChannel: HybridTimelineChannel { HybridTimelineChannel(client: client) }
     public var capture: StreamingCapture { StreamingCapture(client: client) }
 
+    // connect
+
     public func connect() {
         client.connect()
     }
-    public func connect() async {
-        await client.connect()
+    public func connect(_ onConnected: @escaping ([String: String]) -> Void) {
+        client.onConnected(onConnected)
+        connect()
     }
+    @discardableResult
+    public func connect() async -> [String: String] {
+        let headers = await client.connect()
+        return headers
+    }
+
+    // reconnect
+
+    public func reconnect() {
+        Task {
+            await client.reconnect()
+        }
+    }
+    public func reconnect(_ onReconnected: @escaping ([String: String]) -> Void) {
+        Task {
+            let headers = await client.reconnect()
+            onReconnected(headers)
+        }
+    }
+    @discardableResult
+    public func reconnect() async -> [String: String] {
+        return await client.reconnect()
+    }
+
+    // disconnect
+
     public func disconnect() {
         client.disconnect()
     }
+    public func disconnect() async {
+        client.disconnect()
+        while client.getState() == .connected {}
+    }
 
+    // onEvent
+
+    @discardableResult
+    public func onDisconnected(_ callback: @escaping (String, UInt16) -> Void) -> Self {
+        client.onDisconnected(callback)
+        return self
+    }
+    @discardableResult
+    public func onCanceled(_ callback: @escaping () -> Void) -> Self {
+        client.onCanceled(callback)
+        return self
+    }
+    @discardableResult
+    public func onError(_ callback: @escaping (Error?) -> Void) -> Self {
+        client.onError(callback)
+        return self
+    }
+    @discardableResult
+    public func onPeerClosed(_ callback: @escaping () -> Void) -> Self {
+        client.onPeerClosed(callback)
+        return self
+    }
 }
